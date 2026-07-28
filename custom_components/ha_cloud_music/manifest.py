ba8eb6e8da808a423aec59894da94b09de5e22b0
bilezhou <45872939+bilezhou@users.noreply.github.com>
2026-07-28 23:00:12 +0800

    添加 custom_components/ha_cloud_music/manifest.py

---
+++ custom_components/ha_cloud_music/manifest.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from homeassistant.util.json import load_json
+
+def custom_components_path(file_path):
+    return str(Path(__file__).resolve().parent.parent / file_path)
+
+class Manifest():
+
+    def __init__(self, domain):
+        self.domain = domain
+        self.manifest_path = custom_components_path(f'{domain}/manifest.json')
+        self.update()
+
+    @property
+    def remote_url(self):
+        return 'https://gitee.com/shaonianzhentan/ha_cloud_music/raw/dev/custom_components/ha_cloud_music/manifest.json'
+
+    def update(self):
+        data = load_json(self.manifest_path, {})
+        self.domain = data.get('domain')
+        self.name = data.get('name')
+        self.version = data.get('version')
+        self.documentation = data.get('documentation')
+
+manifest = Manifest('ha_cloud_music')
+
